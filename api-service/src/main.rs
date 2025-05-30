3f764133ba9cc06b9a8befd435ab18c797c3fc79
Han Hossain <hanhossain@outlook.com>
2025-05-30 00:20:08 -0500

    simple ping

--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::StatusCode;
+use axum::routing::get;
+use axum::{Json, Router};
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(ping));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn ping() -> (StatusCode, Json<Ping>) {
+    (
+        StatusCode::OK,
+        Json(Ping {
+            ping: "Pong".to_string(),
+        }),
+    )
+}
+
+#[derive(Serialize)]
+struct Ping {
+    ping: String,
 }
