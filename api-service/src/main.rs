3089e26c1958233e3138636b37b2165adc819465
Han Hossain <hanhossain@outlook.com>
2025-05-30 01:30:14 -0500

    simple tracing to console

--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -2,13 +2,28 @@ use axum::http::StatusCode;
 use axum::routing::get;
 use axum::{Json, Router};
 use serde::Serialize;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/ping", get(ping));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .from_env_lossy(),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let app = Router::new()
+        .route("/ping", get(ping))
+        .layer(TraceLayer::new_for_http());
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
