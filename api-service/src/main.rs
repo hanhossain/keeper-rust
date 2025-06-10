418cd2196ce5f0f7f81c7a40ef8764c444a57e6f
Han Hossain <hanhossain@outlook.com>
2025-06-10 01:42:57 -0500

    http.server.request.duration bucket boundaries

--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -118,6 +118,9 @@ async fn main() {
             .f64_histogram("http.server.request.duration")
             .with_description("Duration of HTTP server requests.")
             .with_unit("s")
+            .with_boundaries(vec![
+                0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0,
+            ])
             .build(),
     };
     let middleware = ServiceBuilder::new()
